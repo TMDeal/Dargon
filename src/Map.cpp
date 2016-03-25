011cc8a1b32ff8b5dec0361801e71c440e37469d
Trent Deal <trent.deal@gmail.com>
2016-03-25 15:00:39 -0400

    color background darker/lighter depending on FOV radius

--- src/Map.cpp
+++ src/Map.cpp
@@ -60,11 +60,19 @@ void Map::render() const
 {
     static const TCODColor darkWall(0, 0, 100);
     static const TCODColor darkGround(50, 50, 150);
+    static const TCODColor lightWall(130, 110, 50);
+    static const TCODColor lightGround(200, 180, 50);
 
     for(int x = 0; x < width; x++){
         for(int y = 0; y < height; y++){
-            TCODConsole::root->setCharBackground(x, y,
-                    isWall(x, y) ? darkWall : darkGround);
+            if(isInFov(x, y)){
+                TCODConsole::root->setCharBackground(x, y,
+                        isWall(x, y) ? lightWall : lightGround);
+            }
+            else if(isExplored(x, y)){
+                TCODConsole::root->setCharBackground(x, y,
+                        isWall(x, y) ? darkWall : darkGround);
+            }
         }
     }
 
