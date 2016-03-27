9f38d9895b73d2e3b80657537f94ad24d5b886ac
Trent Deal <trent.deal@gmail.com>
2016-03-27 15:55:50 -0400

    Actors now handle their own movements and FOV status

--- src/Game.cpp
+++ src/Game.cpp
@@ -19,32 +19,58 @@ Game::~Game()
     delete map;
 }
 
+void Game::placeActor(int x, int y, bool playerStart){
+    int roomMonsters = rng->getInt(0, MAX_ROOM_MONSTERS);
+    if(playerStart){
+        player->move(x, y);
+    }
+    if(roomMonsters > 0){
+        if(map->canWalk(x, y)){
+            actors.push(new Actor(x, y, '@', TCODColor::yellow, map));
+        }
+        roomMonsters--;
+    }
+}
+
+void Game::createRoom(bool first, int x1, int y1, int x2, int y2)
+{
+    map->dig(x1, y1, x2, y2);
+    int x;
+    int y;
+    if(first){
+       x = (x1 + x2) / 2;
+       y = (y1 + y2) / 2;
+       placeActor(x, y, first);
+    }
+    else{
+        x = rng->getInt(x1, x2);
+        y = rng->getInt(y1, y2);
+        placeActor(x, y);
+    }
+}
+
 void Game::update()
 {
     TCOD_key_t key;
     TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS, &key, NULL);
     switch(key.vk){
         case TCODK_UP:
-            if(map->canWalk(player->x, player->y-1)){
-                player->y--;
+            if(player->move(UP)){
                 computeFov = true;
             }
             break;
         case TCODK_DOWN:
-            if(map->canWalk(player->x, player->y+1)){
-                player->y++;
+            if(player->move(DOWN)){
                 computeFov = true;
             }
             break;
         case TCODK_LEFT:
-            if(map->canWalk(player->x-1, player->y)){
-                player->x--;
+            if(player->move(LEFT)){
                 computeFov = true;
             }
             break;
         case TCODK_RIGHT:
-            if(map->canWalk(player->x+1, player->y)){
-                player->x++;
+            if(player->move(RIGHT)){
                 computeFov = true;
             }
             break;
@@ -53,7 +79,7 @@ void Game::update()
     }
 
     if(computeFov){
-        map->computeFov();
+        player->computeFov();
         computeFov = false;
     }
 }
@@ -64,9 +90,9 @@ void Game::render()
     map->render();
 
     for(Actor **iter = actors.begin(); iter != actors.end(); iter++){
-        Actor *creature = *iter;
-        if(map->isInFov(creature->x, creature->y)){
-            creature->render();
+        Actor *actor = *iter;
+        if(actor->isInFov()){
+            actor->render();
         }
     }
 }
