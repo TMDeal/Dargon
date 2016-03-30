491340ec9e74ec13ad1c4e78c86e10c952b11f47
Trent Deal <trent.deal@gmail.com>
2016-03-29 23:22:47 -0400

    Defined and implemented a Creature that is derevied from Actor

---
+++ include/Creature.hpp
@@ -0,0 +1,26 @@
+#ifndef CREATURE_HPP
+#define CREATURE_HPP
+
+#include "Actor.hpp"
+#include "Map.hpp"
+#include "Stats.hpp"
+
+class Creature : public Actor{
+    public:
+        Creature(int x, int y, int ch, TCODColor col, Map *map);
+        ~Creature();
+
+        bool move(Direction dir);
+        bool isAlive() const;
+        void heal(int healAmount);
+        void takeDamage(int damage);
+
+        void attack(Creature &defender);
+        int defend(Creature &attacker);
+    private:
+        typedef Actor super;
+    protected:
+        Stats *stats;  // Object to define Actor stats
+};
+
+#endif /* CREATURE_HPP */
---
+++ src/Creature.cpp
@@ -0,0 +1,100 @@
+#include "Creature.hpp"
+
+Creature::Creature(int x, int y, int ch, TCODColor col, Map *map)
+    : super(x, y, ch, col, map)
+{
+}
+
+Creature::~Creature()
+{
+    delete stats;
+}
+
+void Creature::attack(Creature &defender)
+{
+}
+
+int Creature::defend(Creature &attacker)
+{
+}
+
+bool Creature::move(Direction dir)
+{
+    switch(dir){
+        case INVALID_DIRECTION:
+            break;
+        case DOWN_LEFT_DIAG:
+            if(map->canWalk(x-1, y+1)){
+                this->x--;
+                this->y++;
+                return true;
+            }
+            break;
+        case DOWN:
+            if(map->canWalk(x, y+1)){
+                this->y++;
+                return true;
+            }
+            break;
+        case DOWN_RIGHT_DIAG:
+            if(map->canWalk(x+1, y+1)){
+                this->x++;
+                this->y++;
+                return true;
+            }
+            break;
+        case LEFT:
+            if(map->canWalk(x-1, y)){
+                this->x--;
+                return true;
+            }
+            break;
+        case STAY:
+            break;
+        case RIGHT:
+            if(map->canWalk(x+1, y)){
+                this->x++;
+                return true;
+            }
+            break;
+        case UP_LEFT_DIAG:
+            if(map->canWalk(x-1, y-1)){
+                this->x--;
+                this->y--;
+                return true;
+            }
+            break;
+        case UP:
+            if(map->canWalk(x, y-1)){
+                this->y--;
+                return true;
+            }
+            break;
+        case UP_RIGHT_DIAG:
+            if(map->canWalk(x+1, y-1)){
+                this->x--;
+                this->y--;
+                return true;
+            }
+            break;
+        default:
+            break;
+    }
+    return false;
+}
+
+
+bool Creature::isAlive() const
+{
+    return stats->isAlive();
+}
+
+void Creature::heal(int healAmount)
+{
+    stats->heal(healAmount);
+}
+
+void Creature::takeDamage(int damage)
+{
+    stats->takeDamage(damage);
+}
