baeaf81b115472192bd399eda595877ea3ee1d78
Trent Deal <trent.deal@gmail.com>
2016-03-29 23:28:06 -0400

    added pure virtual use method

--- include/Item.hpp
+++ include/Item.hpp
@@ -2,19 +2,18 @@
 #define ITEM_HPP
 
 #include "libtcod/libtcod.hpp"
+#include "Actor.hpp"
+#include "Creature.hpp"
 #include "Map.hpp"
 
-class Item{
+class Item : public Actor{
     public:
         Item(int x, int y, int ch, TCODColor col, Map *map);
         ~Item();
 
-        void render();
-    protected:
-        int x, y;
-        int ch;
-        TCODColor col;
-        Map *map;
+        virtual void use(Creature &creature) = 0;
+    private:
+        typedef Actor super;
 };
 
 #endif /* ITEM_HPP */
