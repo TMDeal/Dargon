b2ce35fa3cb179ff41055dc692bc7e92490bd94e
Trent Deal <trent.deal@gmail.com>
2016-03-30 10:00:56 -0400

    Update player according to key press

--- src/Player.cpp
+++ src/Player.cpp
@@ -10,3 +10,44 @@ Player::~Player()
 {
 }
 
+void Player::die()
+{
+    return;
+}
+
+void Player::update()
+{
+    super::update();
+    if(stats->isAlive()){
+        TCOD_key_t input;
+        TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS, &input, NULL);
+        switch(input.vk){
+            case TCODK_UP:
+                if(move(UP)){
+                    computeFov();
+                }
+                break;
+            case TCODK_DOWN:
+                if(move(DOWN)){
+                    computeFov();
+                }
+                break;
+            case TCODK_LEFT:
+                if(move(LEFT)){
+                    computeFov();
+                }
+                break;
+            case TCODK_RIGHT:
+                if(move(RIGHT)){
+                    computeFov();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    else{
+        return;
+    }
+}
+
