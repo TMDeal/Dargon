f17c3582e63d4bb50add26389abb8cca19054654
Trent Deal <trent.deal@gmail.com>
2016-04-06 16:47:44 -0400

    Set Actor on a tile during constructor

--- src/Dargon.cpp
+++ src/Dargon.cpp
@@ -4,6 +4,7 @@ Dargon::Dargon(int x, int y, Map *map)
     : super(x, y, 'D', TCODColor::lightBlue, map)
 {
     this->stats = new Stats(10, 10, 5, 5);
+    map->setActorOnTile(*this, x, y);
 }
 
 void Dargon::die()
--- src/Player.cpp
+++ src/Player.cpp
@@ -4,6 +4,7 @@ Player::Player(int x, int y, Map *map)
     : super(x, y, '@', TCODColor::white, map)
 {
     this->stats = new Stats(10, 10, 5, 5);
+    map->setActorOnTile(*this, x, y);
 }
 
 Player::~Player()
