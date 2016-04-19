25f231f4981fe4a6d3227dd1991ed5fdcad86a92
Trent Deal <trent.deal@gmail.com>
2016-04-19 14:27:33 -0400

    Remove operator overloads
    Cooridnate is now much simpler. Its only purpose now is to provide a
    compass of directions that have an x and y value.

--- include/Coordinates.hpp
+++ include/Coordinates.hpp
@@ -4,43 +4,6 @@
 typedef struct Coordinate{
     int x;
     int y;
-
-    Coordinate(int newX, int newY) : x(newX), y(newY){}
-    Coordinate() : x(0), y(0){}
-
-    void set(int newX, int newY){
-        x = newX;
-        y = newY;
-    }
-
-    bool operator ==(const Coordinate &coor) const{
-        return this->x == coor.x && this->y == coor.y;
-    }
-
-    bool operator !=(const Coordinate &coor) const{
-        return !operator == (coor); 
-    }
-
-    bool operator <(const Coordinate &coor) const{
-        return this->x < coor.x || (this->x == coor.x && this->y < coor.y);
-    }
-
-    bool operator >(const Coordinate &coor) const{
-        return this->x > coor.x || (this->x == coor.x && this->y > coor.y);
-    }
-
-    const Coordinate &operator +=(const Coordinate &coor){
-        this->x += coor.x;
-        this->y += coor.y;
-        return *this;
-    }
-
-    const Coordinate &operator -=(const Coordinate &coor){
-        this->x -= coor.x;
-        this->y -= coor.y;
-        return *this;
-    }
-
 }Coordinate;
 
 const struct Coordinate Directions[9]={
