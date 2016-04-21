1be92ad6e618b32f2b2fb9ad4531f3f83d49d145
Trent Deal <trent.deal@gmail.com>
2016-04-21 09:54:54 -0400

    Create class for the Gui
    moved TCODConsole::root functions over to this new Gui class, as well as
    adding a new status window that displays logs and player stats. also
    displays stats of tile under cursor if it has a monster, item, or player
    
    the logs work currently, but displaying player stats is not yet
    implemented.

---
+++ include/Gui.hpp
@@ -0,0 +1,41 @@
+#ifndef GUI_HPP
+#define GUI_HPP
+
+#include <vector>
+#include <string>
+#include <cstdarg>
+#include <cstring>
+
+#include "libtcod/libtcod.hpp"
+
+typedef struct Message{
+    std::string text;
+    TCODColor col;
+
+    Message(std::string text, TCODColor col)
+        :text(text), col(col){}
+}Message;
+
+typedef std::vector<Message*>::iterator logIter;
+
+class Gui{
+    public:
+        Gui(int screenWidth, int screenHeight);
+        ~Gui();
+        void render();
+        void renderLog();
+        void renderMouseLook();
+        void addLog(const TCODColor &col, std::string fmt, ...);
+
+        const static int PANEL_HEIGHT = 7;
+        const static int BAR_WIDTH = 20;
+        const static int MSG_X=BAR_WIDTH+2;
+        const static int MSG_HEIGHT=PANEL_HEIGHT-1;
+
+    private:
+        TCODConsole *con;
+        int screenWidth, screenHeight;
+        std::vector<Message*> logs;
+};
+
+#endif /* GUI_HPP */
---
+++ src/Gui.cpp
@@ -0,0 +1,91 @@
+#include "Gui.hpp"
+#include "Game.hpp"
+#include "templates_out/projectPaths.h"
+#include "algorithm"
+
+Gui::Gui(int screenWidth, int screenHeight)
+    : screenWidth(screenWidth), screenHeight(screenHeight)
+{
+    TCODConsole::setCustomFont((getResPath() + "terminal.png").c_str());
+    TCODConsole::initRoot(screenWidth, screenHeight, "Dargon", false);
+    this->con = new TCODConsole(screenWidth, PANEL_HEIGHT);
+
+    logs.push_back(new Message("WELCOME", TCODColor::lightGrey));
+}
+
+Gui::~Gui()
+{
+}
+
+void Gui::addLog(const TCODColor &col, std::string fmt, ...)
+{
+    va_list args;
+    char buffer[256];
+    va_start(args, fmt);
+    vsprintf(buffer, fmt.c_str(), args);
+    va_end(args);
+
+    char *lineBegin = buffer;
+    char *lineEnd;
+
+    do{
+        if(logs.size() == MSG_HEIGHT){
+            Message *tmp = logs.at(0);
+            logs.erase(std::remove(logs.begin(), logs.end(), tmp), logs.end());
+            delete tmp;
+        }
+
+        lineEnd = strchr(lineBegin, '\n');
+        if(lineEnd){
+            *lineEnd = '\0';
+        }
+
+        logs.push_back(new Message(string(lineBegin), col));
+    }while(lineEnd);
+}
+
+void Gui::renderMouseLook()
+{
+    if(!game.levelMap->isInFov(game.mouse.cx, game.mouse.cy)){
+        return;
+    }
+    std::string buffer;
+    switch(game.tiles[game.mouse.cx][game.mouse.cy].flag){
+        case HAS_MONSTER:
+            buffer = game.find<Monster>(game.monsters, game.mouse.cx, game.mouse.cy).getInfo();
+            break;
+        case HAS_PLAYER:
+            buffer = game.player->getInfo();
+        case HAS_ITEM:
+            break;
+        case SAFE:
+            break;
+    }
+    con->setDefaultBackground(TCODColor::lightGrey);
+    con->print(1, 0, buffer.c_str());
+}
+
+void Gui::renderLog()
+{
+    int y = 1;
+    float colorFade = 0.4f;
+    for(logIter iter = logs.begin(); iter != logs.end(); iter++){
+        Message *message = *iter;
+        con->setDefaultForeground(message->col * colorFade);
+        con->print(MSG_X, y, message->text.c_str());
+        y++;
+        if(colorFade < 1.0f){
+            colorFade +=0.3f;
+        }
+    }
+}
+
+void Gui::render()
+{
+    con->setDefaultBackground(TCODColor::black);
+    con->clear();
+    renderLog();
+    renderMouseLook();
+    TCODConsole::blit(con, 0, 0, screenWidth, PANEL_HEIGHT,
+            TCODConsole::root, 0, screenHeight - PANEL_HEIGHT);
+}
