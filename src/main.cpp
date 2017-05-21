95ce79274e5368cb31d40cdfb11bad08d084c0f2
Trent Deal <trent.deal@gmail.com>
2017-05-21 19:32:44 -0400

    Cap frames according to config

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,25 @@
+#include <SDL_timer.h>
 #include <iostream>
 #include "config.hpp"
 #include "system.hpp"
 
+Config config = getConfig("Dargon");
+const Uint32 TICKS_PER_SECOND = 1000 / config.fps;
+
 int main(int argc, char *argv[])
 {
-    Config config = getConfig("Dargon");
     System sys(config);
 
     while(sys.running()) {
+        Uint32 start_ticks = SDL_GetTicks();
+
         sys.update();
         sys.draw();
+
+        Uint32 deltaTime = SDL_GetTicks() - start_ticks;
+        if(deltaTime < TICKS_PER_SECOND) {
+            SDL_Delay(TICKS_PER_SECOND - deltaTime);
+        }
     }
 
     return EXIT_SUCCESS;
