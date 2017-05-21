6cf689a579837d63f6071bea8474a09963411e45
Trent Deal <trent.deal@gmail.com>
2017-05-21 13:16:44 -0400

    Default to white color

--- include/color.hpp
+++ include/color.hpp
@@ -11,7 +11,7 @@ struct Color {
     uint8_t b;
     uint8_t a;
 
-    Color() : r(0), g(0), b(0), a(255) {}
+    Color() : r(255), g(255), b(255), a(255) {}
     Color(uint8_t r, uint8_t g, uint8_t b) : r(r), g(g), b(b), a(255) {}
     Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a) : r(r), g(g), b(b), a(a) {}
 
