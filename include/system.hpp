9c14887dbc019ad22ba8d05954f21a05caece7aa
Trent Deal <trent.deal@gmail.com>
2017-05-21 14:09:18 -0400

    Forward declare SDL2pp objects, rename handleEvents

--- include/system.hpp
+++ include/system.hpp
@@ -1,33 +1,39 @@
 #ifndef SYSTEM_HPP
 #define SYSTEM_HPP
 
-#include <SDL2pp/SDL.hh>
-#include <SDL2pp/SDLImage.hh>
-#include <SDL2pp/SDLTTF.hh>
-#include <SDL2pp/Mixer.hh>
-#include <SDL2pp/Window.hh>
-#include <SDL2pp/Renderer.hh>
-
+#include <memory>
 #include "config.hpp"
 #include "sprite.hpp"
+#include "player.hpp"
+
+namespace SDL2pp {
+    class SDL;
+    class SDLImage;
+    class SDLTTF;
+    class Mixer;
+    class Window;
+    class Renderer;
+}
 
 class System {
     private:
         bool m_running;
         Config m_config;
 
-        SDL2pp::SDL m_sdl;
-        SDL2pp::SDLImage m_sdl_image;
-        SDL2pp::SDLTTF m_sdl_ttf;
-        SDL2pp::Mixer m_mixer;
-        SDL2pp::Window m_window;
-        SDL2pp::Renderer m_renderer;
+        std::unique_ptr<SDL2pp::SDL> m_sdl;
+        std::unique_ptr<SDL2pp::SDLImage> m_sdl_image;
+        std::unique_ptr<SDL2pp::SDLTTF> m_sdl_ttf;
+        std::unique_ptr<SDL2pp::Mixer> m_mixer;
+        std::unique_ptr<SDL2pp::Window> m_window;
+        std::unique_ptr<SDL2pp::Renderer> m_renderer;
 
         SpriteSheet m_sprite_sheet;
+
+        Player m_player;
     public:
         System(const Config& config);
+        ~System();
 
-        void handleEvents();
         void update();
         void draw();
 
--- src/system.cpp
+++ src/system.cpp
@@ -1,43 +1,62 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_mixer.h>
+#include <SDL2pp/SDL.hh>
+#include <SDL2pp/SDLImage.hh>
+#include <SDL2pp/SDLTTF.hh>
+#include <SDL2pp/Mixer.hh>
+#include <SDL2pp/Window.hh>
+#include <SDL2pp/Renderer.hh>
+#include <SDL2pp/Optional.hh>
 #include "system.hpp"
+#include "direction.hpp"
+
+using SDL2pp::NullOpt;
+using SDL2pp::SDL;
+using SDL2pp::SDLImage;
+using SDL2pp::SDLTTF;
+using SDL2pp::Mixer;
+using SDL2pp::Window;
+using SDL2pp::Renderer;
 
 System::System(const Config& config)
     :m_running(true),
     m_config(config),
-    m_sdl(SDL_INIT_VIDEO | SDL_INIT_AUDIO),
-    m_sdl_image(IMG_INIT_PNG),
-    m_sdl_ttf(),
-    m_mixer(MIX_DEFAULT_FREQUENCY,
+    m_sdl(std::make_unique<SDL>(SDL_INIT_VIDEO | SDL_INIT_AUDIO)),
+    m_sdl_image(std::make_unique<SDLImage>(IMG_INIT_PNG)),
+    m_sdl_ttf(std::make_unique<SDLTTF>()),
+    m_mixer(std::make_unique<Mixer>(MIX_DEFAULT_FREQUENCY,
             MIX_DEFAULT_FORMAT,
-            MIX_DEFAULT_CHANNELS, 4096),
-    m_window("Dargon", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-            m_config.screen_width, m_config.screen_height, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE),
-    m_renderer(m_window, -1, SDL_RENDERER_ACCELERATED),
-    m_sprite_sheet(m_renderer, "sprites")
-{ }
+            MIX_DEFAULT_CHANNELS, 4096)),
+    m_window(std::make_unique<Window>("Dargon", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+            m_config.screen_width, m_config.screen_height, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE)),
+    m_renderer(std::make_unique<Renderer>(*m_window.get(), -1, SDL_RENDERER_ACCELERATED)),
+    m_sprite_sheet(*m_renderer.get(), "sprites"),
+    m_player("Rogue", m_sprite_sheet.makeSprite("male-default", Color(0, 0, 255), NullOpt))
+{}
 
-void System::handleEvents() {
+System::~System() = default;
+
+void System::update() {
     SDL_Event event;
 
     while(SDL_PollEvent(&event)) {
         switch(event.type) {
             case SDL_QUIT:
                 quit();
+            case SDL_KEYDOWN:
+                m_player.update(event);
             default:
                 break;
         }
     }
 }
 
-void System::update() {
-    return;
-}
-
 void System::draw() {
-    m_renderer.SetDrawColor(0, 0, 0).Clear();
-    m_renderer.Present();
+    m_renderer->SetDrawColor(0, 0, 0);
+    m_renderer->Clear();
+    m_player.render(*m_renderer.get());
+    m_renderer->Present();
 }
 
 bool System::running() const {
